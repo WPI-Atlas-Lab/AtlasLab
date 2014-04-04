8881e1d94fa60048b67a6506ce926d2079e522c0
Matt DeDonato <mpdedonato@wpi.edu>
2014-04-04 11:16:53 -0400

    added -N option so that it overwrites file

--- setup_scripts/install.bash
+++ setup_scripts/install.bash
@@ -8,36 +8,36 @@ if [ $(whoami) == 'root' ];then
 fi
 
 cd ~/Downloads
-wget https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/video_card_install.bash
+wget -N https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/video_card_install.bash
 sudo chmod +x video_card_install.bash
 ./video_card_install.bash
 
 cd ~/Downloads
-wget https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/ros_drcsim_install.bash
+wget -N https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/ros_drcsim_install.bash
 sudo chmod +x ros_drcsim_install.bash
 ./ros_drcsim_install.bash
 
 cd ~/Downloads
-wget https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/workspace_setup.bash
+wget -N https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/workspace_setup.bash
 sudo chmod +x workspace_setup.bash
 ./workspace_setup.bash
 
 cd ~/Downloads
-wget https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/dependancy_install.bash
+wget -N https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/dependancy_install.bash
 sudo chmod +x dependancy_install.bash
 ./dependancy_install.bash
 
 cd ~/Downloads
-wget https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/flycapture_install.bash
+wget -N https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/flycapture_install.bash
 sudo chmod +x flycapture_install.bash
 ./flycapture_install.bash
 
 cd ~/Downloads
-wget https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/useful_software_install.bash
+wget -N https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/useful_software_install.bash
 sudo chmod +x useful_software_install.bash
 ./useful_software_install.bash
 
 cd ~/Downloads
-wget https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/printer_install.bash
+wget -N https://raw.github.com/WPI-Atlas-Lab/AtlasLab/master/setup_scripts/printer_install.bash
 sudo chmod +x printer_install.bash
 sudo ./printer_install.bash
