5e7e205a59dea8af6f0e75c2cc439e2bbcdc6984
Matt DeDonato <mpdedonato@wpi.edu>
2014-04-04 13:36:45 -0400

    Update dependancy_install.bash

--- setup_scripts/dependancy_install.bash
+++ setup_scripts/dependancy_install.bash
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 sudo apt-get -y install ros-hydro-qt-ros
-sudo apt-get -y -f install
-sudo apt-get -y update
-sudo apt-get -y install libraw1394-11 libgtk2.0-0 libgtkmm-2.4-dev libglademm-2.4-dev libgtkglextmm-x11-1.2-dev libusb-1.0-0
 sudo apt-get -y install ros-hydro-razer-hydra
 sudo apt-get -y install ros-hydro-camera-info-manager-py
 sudo apt-get -y install libbobcat-dev
