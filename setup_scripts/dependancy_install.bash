108877a2064fc172ab92167b4136e99e14353635
Matt DeDonato <mpdedonato@wpi.edu>
2014-06-18 11:29:02 -0400

    Update dependancy_install.bash

--- setup_scripts/dependancy_install.bash
+++ setup_scripts/dependancy_install.bash
@@ -6,3 +6,6 @@ sudo apt-get -y install ros-hydro-camera-info-manager-py
 sudo apt-get -y install libbobcat-dev
 sudo apt-get -y install libx264-120
 sudo apt-get -y install  ros-hydro-ar-track-alvar
+sudo apt-get -y install python-pip
+
+sudo pip install  -U pymodbus
