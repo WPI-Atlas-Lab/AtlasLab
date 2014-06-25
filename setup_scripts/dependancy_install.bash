70485810545f7db80202b7023f2fe169227a4b70
Matt DeDonato <mpdedonato@wpi.edu>
2014-06-25 18:10:46 -0400

    Update dependancy_install.bash

--- setup_scripts/dependancy_install.bash
+++ setup_scripts/dependancy_install.bash
@@ -5,6 +5,8 @@ sudo apt-get -y install ros-hydro-qt-ros
 sudo apt-get -y install ros-hydro-razer-hydra
 sudo apt-get -y install ros-hydro-camera-info-manager-py
 sudo apt-get -y install libbobcat-dev
+sudo apt-get -y install ros-hydro-moveit-ros
+sudo apt-get -y install ros-hydro-fcl
 sudo apt-get -y install libx264-120
 sudo apt-get -y install  ros-hydro-ar-track-alvar
 sudo apt-get -y install python-pip
