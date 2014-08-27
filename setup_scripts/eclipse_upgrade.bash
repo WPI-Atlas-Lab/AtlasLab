f82685687c33d634e31693cbda0d3636216b38fd
Matt DeDonato <matt.dedonato@gmail.com>
2014-08-27 18:05:44 -0400

    Update eclipse_upgrade.bash

--- setup_scripts/eclipse_upgrade.bash
+++ setup_scripts/eclipse_upgrade.bash
@@ -1,5 +1,5 @@
 cd ~/Downloads/
 sudo rm -rf /opt/eclipse
-wget http://ftp.osuosl.org/pub/eclipse//technology/epp/downloads/release/kepler/SR1/eclipse-cpp-kepler-SR1-linux-gtk-x86_64.tar.gz
-tar xvf eclipse-cpp-kepler-SR1-linux-gtk-x86_64.tar.gz 
+wget http://mirrors.ibiblio.org/pub/mirrors/eclipse/technology/epp/downloads/release/luna/R/eclipse-cpp-luna-R-linux-gtk-x86_64.tar.gz
+tar xvf eclipse-cpp-luna-R-linux-gtk-x86_64.tar.gz
 sudo mv eclipse /opt/
