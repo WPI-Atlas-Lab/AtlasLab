b09547c96d9520178b1b7066c10e3af7332cfbcb
Matt DeDonato <matt.dedonato@gmail.com>
2014-08-28 09:30:37 -0400

    Update eclipse_upgrade.bash

--- setup_scripts/eclipse_upgrade.bash
+++ setup_scripts/eclipse_upgrade.bash
@@ -1,5 +1,5 @@
 cd ~/Downloads/
-wget http://mirrors.ibiblio.org/pub/mirrors/eclipse/technology/epp/downloads/release/luna/R/eclipse-cpp-luna-R-linux-gtk-x86_64.tar.gz
+wget -N http://mirrors.ibiblio.org/pub/mirrors/eclipse/technology/epp/downloads/release/luna/R/eclipse-cpp-luna-R-linux-gtk-x86_64.tar.gz
 tar xvf eclipse-cpp-luna-R-linux-gtk-x86_64.tar.gz
 sudo rm -rf /opt/eclipse
 sudo mv eclipse /opt/
