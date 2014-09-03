fc97ce066ff822c05912e936c2dec0bb50465604
Matt DeDonato <matt.dedonato@gmail.com>
2014-09-03 10:31:49 -0400

    Update video_card_install.bash

--- setup_scripts/video_card_install.bash
+++ setup_scripts/video_card_install.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:ubuntu-x-swat/x-updates 
-sudo apt-get -y update
+ sudo add-apt-repository ppa:xorg-edgers/ppa
+ sudo apt-get -y update
 sudo apt-get -y install nvidia-331
