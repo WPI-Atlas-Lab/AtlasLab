ea3c7101e93f5b35d958db2a9c2b7bfef9dcfa00
Felipe Polido <felipe.polido@gmail.com>
2014-10-07 15:53:41 -0400

    fix fix fix2

--- setup_scripts/openrave_setup.bash
+++ setup_scripts/openrave_setup.bash
@@ -5,6 +5,7 @@ echo export PYTHONPATH >> ~/.bashrc
 echo export GUROBI_HOME="/opt/gurobi563/linux64" >> ~/.bashrc
 echo export PATH="${PATH}:${GUROBI_HOME}/bin" >> ~/.bashrc
 echo export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib >> ~/.bashrc
+echo alias drctrajopt="'cd ~/drc_workspace/src/trajopt/'" >> ~/.bashrc
 . ~/.bashrc
 
 cd ~/drc_workspace/src/trajopt
