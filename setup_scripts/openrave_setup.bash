49a974034b816f2dbee5f122e15602c1a939a534
Felipe Polido <felipe.polido@gmail.com>
2014-10-07 16:01:04 -0400

    fix fix fix3

--- setup_scripts/openrave_setup.bash
+++ setup_scripts/openrave_setup.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo PYTHONPATH="${PYTHONPATH}:~/drc_workspace/src/trajopt/build_trajopt/lib:~/drc_workspace/src/trajopt/src/trajopt" >> ~/.bashrc
+echo PYTHONPATH="${PYTHONPATH}:~/drc_workspace/src/trajopt/build_trajopt/lib:~/drc_workspace/src/trajopt" >> ~/.bashrc
 echo export PYTHONPATH >> ~/.bashrc
 echo export GUROBI_HOME="/opt/gurobi563/linux64" >> ~/.bashrc
 echo export PATH="${PATH}:${GUROBI_HOME}/bin" >> ~/.bashrc
@@ -13,3 +13,14 @@ mkdir build_trajopt
 cd build_trajopt 
 cmake -DBUILD_HUMANOIDS=ON -DBUILD_CLOUDPROC=ON ../
 make -j
+
+#Download gurobi5.6.3:
+#http://www.gurobi.com/download/gurobi-optimizer
+
+#cd ~/Downloads
+#tar xvfz gurobi5.6.3_linux64.tar.gz
+
+#sudo rm -rf /opt/gurobi563
+#sudo mv gurobi563 /opt/
+#Getting a license:
+#http://www.gurobi.com/documentation/5.6/quick-start-guide/how_to_obtain_and_install_
