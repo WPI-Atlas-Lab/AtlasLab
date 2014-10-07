6a276ace3886a0865b287a7d4177009557965793
Felipe Polido <felipe.polido@gmail.com>
2014-10-07 15:42:07 -0400

    Update

--- setup_scripts/openrave_setup.bash
+++ setup_scripts/openrave_setup.bash
@@ -2,7 +2,9 @@
 
 echo PYTHONPATH="${PYTHONPATH}:~/drc_workspace/src/trajopt/build_trajopt/lib:~/drc_workspace/src/trajopt/src/trajopt" >> ~/.bashrc
 echo export PYTHONPATH >> ~/.bashrc
-echo export GUROBI_HOME="/opt/gurobi563/linux64">> ~/.bashrc
+echo export GUROBI_HOME="/opt/gurobi563/linux64" >> ~/.bashrc
+echo export PATH="${PATH}:${GUROBI_HOME}/bin" >> ~/.bashrc
+echo export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib >> ~/.bashrc
 . ~/.bashrc
 
 cd ~/drc_workspace/src/trajopt
