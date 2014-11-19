11d7f8e604f64c2e217df23cc8a23f20596719d7
Matt DeDonato <matt.dedonato@gmail.com>
2014-11-19 11:18:00 -0500

    Update openrave_install.bash

--- setup_scripts/openrave_install.bash
+++ setup_scripts/openrave_install.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt-get install libopenscenegraph-dev cmake libboost-all-dev libeigen3-dev python-numpy python3.2-dev
-sudo add-apt-repository ppa:openrave/testing
+sudo apt-get install -y libopenscenegraph-dev cmake libboost-all-dev libeigen3-dev python-numpy python3.2-dev
+sudo add-apt-repository -y ppa:openrave/testing
 sudo apt-get -y update
-sudo apt-get install openrave
+sudo apt-get install -y openrave
 sudo apt-get -y update
 
 
