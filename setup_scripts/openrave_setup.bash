c660b468a1dbdae45a9a60004030bb8eef0331d5
felipepolido <felipe.polido@gmail.com>
2014-12-16 11:06:14 -0500

    Switched to new GUROBI version

--- setup_scripts/openrave_setup.bash
+++ setup_scripts/openrave_setup.bash
@@ -2,7 +2,7 @@
 
 echo export PYTHONPATH=~/drc_workspace/src/drc/trajopt/build_trajopt/lib:~/drc_workspace/src/drc/trajopt:'${PYTHONPATH}' >> ~/.bashrc
 echo export LD_LIBRARY_PATH='${LD_LIBRARY_PATH}':/usr/lib >> ~/.bashrc
-echo export GUROBI_HOME="/opt/gurobi563/linux64" >> ~/.bashrc
+echo export GUROBI_HOME="/opt/gurobi600/linux64" >> ~/.bashrc
 echo export OPENRAVE_DATA='${OPENRAVE_DATA}':~/drc_workspace/src/drc/trajopt/ >> ~/.bashrc
 echo alias drctrajopt="'cd ~/drc_workspace/src/drc/trajopt/'" >> ~/.bashrc
 . ~/.bashrc
@@ -15,15 +15,15 @@ echo alias drctrajopt="'cd ~/drc_workspace/src/drc/trajopt/'" >> ~/.bashrc
 
 #. ~/.bashrc
 
-#Download gurobi5.6.3:
+#Download gurobi6.0.0:
 #http://www.gurobi.com/download/gurobi-optimizer
 
 #cd ~/Downloads
-#tar xvfz gurobi5.6.3_linux64.tar.gz
+#tar xvfz gurobi6.0.0_linux64.tar.gz
 
-#sudo rm -rf /opt/gurobi563
-#sudo mv gurobi563 /opt/
+#sudo rm -rf /opt/gurobi600
+#sudo mv gurobi600 /opt/
 #Getting a license:
 #http://www.gurobi.com/documentation/5.6/quick-start-guide/how_to_obtain_and_install_
-#cd /opt/gurobi563/linux64/bin
+#cd /opt/gurobi600/linux64/bin
 #./grbgetkey <LICENSE NUMBER GOES HERE>
